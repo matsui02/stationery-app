e9a65533d3d72e73dcf36356b2f5dd5322a1bc0a
matsui <matuitetta@gmail.com>
2026-07-20 06:38:10 +0900

    Toastifyを使用したフラッシュメッセージを追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
-    Rails.logger.debug "★ registrations create called ★"
     build_resource(sign_up_params)
 
     if resource.save
       sign_up(resource_name, resource)
+      flash[:notice] = "登録が完了しました。"  # ← 追加
       redirect_to after_sign_up_path_for(resource)
     else
-      Rails.logger.debug "=== ERROR ==="
-      Rails.logger.debug resource.errors.full_messages.inspect
       @register_resource = resource
       @login_resource = User.new
 
