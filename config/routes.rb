3b00b117d1d9f1f40c409a0849c6fe36198da874
matsui <matuitetta@gmail.com>
2026-08-11 15:14:46 +0900

    feat: update sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "top#index"
   devise_for :users
-  get '/accounts', to: 'accounts#show', as: :accounts
-  resources :pencil_cases, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  get "/accounts", to: "accounts#show", as: :accounts
+  resources :pencil_cases, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
   namespace :api do
     get "items/search", to: "items#search"
     post "items",        to: "items#create"
