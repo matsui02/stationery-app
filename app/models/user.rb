27f88a4ab7b320ae320ac35f89a3f41516b2644f
matsui <matuitetta@gmail.com>
2026-08-13 14:53:24 +0900

    test: Userモデルの単体テストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+
   validates :name, presence: true
+
+  validates_length_of :password, minimum: 8
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name]
