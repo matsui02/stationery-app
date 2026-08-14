9a9efd59871564b40be83a46246ce6a87fbb6958
matsui <matuitetta@gmail.com>
2026-08-14 17:51:21 +0900

    test: PencilCaseItemモデルの単体テストを追加

--- test/models/pencil_case_item_test.rb
+++ test/models/pencil_case_item_test.rb
@@ -1,4 +1,9 @@
 require "test_helper"
 
 class PencilCaseItemTest < ActiveSupport::TestCase
+  test "アイテムが未選択の場合は作成できない" do
+    pencil_case_item = build(:pencil_case_item, item: nil)
+
+    refute pencil_case_item.valid?
+  end
 end
